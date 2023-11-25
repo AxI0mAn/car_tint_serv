64469559eec7d73ebf767b7ff019bd6171fafd92
AxI0mAn <z1979ua@gmail.com>
2023-11-25 13:42:18 +0200

    swipe script

---
+++ src/scripts/gallery_swipe.js
@@ -0,0 +1,11 @@
+// slider / gallery
+// slider - list / gallery_conteiner
+// slider - track / gallery_img
+// slides / img
+
+// slider - arrows / gallery_header/gallery_control
+// prev / left
+// next / right
+// transfrom: translate3d(Xpx, 0px, 0px); / transform: translateX(0px);
+
+
