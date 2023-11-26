6cd3f61eebb49634c5c041c5f019066a106f9c47
AxI0mAn <z1979ua@gmail.com>
2023-11-26 10:56:16 +0200

    add swiperjs from gallery

--- src/index.js
+++ src/index.js
@@ -2,11 +2,39 @@ import './index.scss';
 import './styles/base/adapt_media.scss';
 import video_control from './scripts/video_control';
 import modal from './scripts/modal';
-// import gallery_swipe from './scripts/gallery_swipe';
-import gallery from './scripts/gallery';
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 video_control('.btn_video0', 0);
 video_control('.btn_video1', 1);
 modal();
-// gallery_swipe();
-gallery();
+
+const swiper = new Swiper('.gallery_conteiner', {
+  modules: [Navigation], // NOTE! configure Swiper to use modules
+  speed: 400,
+  loop: false,
+  navigation: {
+    nextEl: '.right',
+    prevEl: '.left',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    575: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+  },
+});
