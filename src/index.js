1b6d45d3a1c0c015135ac22933a4d7d2eb206411
AxI0mAn <z1979ua@gmail.com>
2023-11-26 18:15:17 +0200

    final swiper

--- src/index.js
+++ src/index.js
@@ -11,13 +11,26 @@ video_control('.btn_video0', 0);
 video_control('.btn_video1', 1);
 modal();
 
-const swiper = new Swiper('.gallery_conteiner', {
+const swiper = new Swiper('.swiper', {
   modules: [Navigation], // NOTE! configure Swiper to use modules
-  speed: 400,
-  loop: false,
   navigation: {
     nextEl: '.right',
     prevEl: '.left',
+  },
+  speed: 800,
+  loop: true,
+  slideToClickedSlide: true,
+  // centeredSlides: true,
+  centeredSlidesBounds: true,
+  breakpointsBase: 'window',
+  slidesPerGroup: 1,
+  spaceBetween: 10,
+  longSwipes: false,
+  // slidesPerView: 2.2,
+  // autoHeight: true,
+  freeMode: {
+    enabled: true,
+    sticky: true,
   },
   breakpoints: {
     320: {
@@ -25,15 +38,15 @@ const swiper = new Swiper('.gallery_conteiner', {
       spaceBetween: 20,
     },
     575: {
-      slidesPerView: 2,
+      slidesPerView: 2.2,
       spaceBetween: 20,
     },
     640: {
-      slidesPerView: 3,
+      slidesPerView: 3.2,
       spaceBetween: 20,
     },
     992: {
-      slidesPerView: 3,
+      slidesPerView: 4.2,
       spaceBetween: 20,
     },
   },
