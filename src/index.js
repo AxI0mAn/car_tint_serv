ea07e7c27eb4b4b87b4468e9e076fc819e9e7b3e
AxI0mAn <124360273+AxI0mAn@users.noreply.github.com>
2023-11-27 17:03:15 +0200

    Car tint serv swiper (#1)
    * swipe script
    
    * test gallery swipe
    
    * test swiper
    
    * old gallery
    
    * add swiperjs from gallery
    
    * final swiper
    
    * README redact
    
    * current edit
    
    ---------
    
    Co-authored-by: AxI0mAn <z1979ua@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,9 +2,52 @@ import './index.scss';
 import './styles/base/adapt_media.scss';
 import video_control from './scripts/video_control';
 import modal from './scripts/modal';
-import gallery from './scripts/gallery';
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 video_control('.btn_video0', 0);
 video_control('.btn_video1', 1);
 modal();
-gallery();
+
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation], // NOTE! configure Swiper to use modules
+  navigation: {
+    nextEl: '.right',
+    prevEl: '.left',
+  },
+  speed: 800,
+  loop: true,
+  slideToClickedSlide: true,
+  // centeredSlides: true,
+  centeredSlidesBounds: true,
+  breakpointsBase: 'window',
+  slidesPerGroup: 1,
+  spaceBetween: 10,
+  longSwipes: false,
+  // slidesPerView: 2.2,
+  // autoHeight: true,
+  freeMode: {
+    enabled: true,
+    sticky: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    575: {
+      slidesPerView: 2.2,
+      spaceBetween: 20,
+    },
+    640: {
+      slidesPerView: 3.2,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 4.2,
+      spaceBetween: 20,
+    },
+  },
+});
